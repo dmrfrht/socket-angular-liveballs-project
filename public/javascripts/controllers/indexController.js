cda29a599d4c49d69038baadf68407b6d6b5ea10
dmrfrht <dmrfrht1@gmail.com>
2021-02-24 14:44:07 +0300

    create newUser and all users array

--- public/javascripts/controllers/indexController.js
+++ public/javascripts/controllers/indexController.js
@@ -1,10 +1,22 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
-  const connectionOptions = {
-    reconnectionAttempts: 3,
-    reconnectionDelay: 600
+
+  $scope.init = () => {
+    const userName = prompt("Please enter username")
+
+    if (userName) initSocket(userName)
+    else return false
   }
 
-  indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-    .then((socket) => console.log("Bağlantı gerçekleşti: ", socket))
-    .catch((error) => console.log(error))
+  function initSocket(userName) {
+    const connectionOptions = {
+      reconnectionAttempts: 3,
+      reconnectionDelay: 600
+    }
+
+    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+      .then((socket) => {
+        socket.emit('newuser', { userName })
+      })
+      .catch((error) => console.log(error))
+  }
 }])
