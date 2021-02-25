bdf79ba824eebd0e415a3605c1ce0dd4c35c5996
dmrfrht <dmrfrht1@gmail.com>
2021-02-25 22:10:26 +0300

    different color assignment to each player

--- src/socketApi.js
+++ src/socketApi.js
@@ -6,6 +6,9 @@ socketApi.io = io
 
 const users = {}
 
+// helpers
+const randomColor = require('../helpers/randomColor')
+
 io.on('connection', (socket) => {
   socket.on('newuser', (data) => {
     const defaultData = {
@@ -13,7 +16,8 @@ io.on('connection', (socket) => {
       position: {
         x: 0,
         y: 0
-      }
+      },
+      color: randomColor()
     }
 
     const userData = Object.assign(data, defaultData)
